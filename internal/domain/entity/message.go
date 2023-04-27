be2ca219771738b11214ff17793f41c7f45e993e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-26 23:30:38 -0300

    :sparkles: create: fun -  NewMessage (.../entity/Message)

--- internal/domain/entity/message.go
+++ internal/domain/entity/message.go
@@ -1,7 +1,11 @@
 package entity
 
-import "time"
+import (
+	"time"
 
+	"github.com/google/uuid"
+	tiktoken_go "github.com/j178/tiktoken-go"
+)
 type Message struct {
 	ID        string
 	Role      string
@@ -12,3 +16,18 @@ type Message struct {
 }
 
 
+func NewMessage(role, content string, model *Model) (*Message, error) {
+  totalTokens := tiktoken_go.CountTokens(model.GetModelName(), content)
+  msg := &Message {
+    ID: uuid.New().String(),
+    Role: role,
+    Content: content,
+    Tokens: totalTokens,
+    Model: model,
+    CreatedAt: time.Now(),
+  }
+
+  if err := msg.Validate(); err != nil { return nil,err }
+
+  return msg, nil
+}
