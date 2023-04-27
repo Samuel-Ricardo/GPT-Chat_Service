f123b35eb9c2f4c9e010aac4de35d2c08cee5e5a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-26 23:09:52 -0300

    :sparkles: create: fun -  NewModel (.../entity/Model)

--- internal/domain/entity/model.go
+++ internal/domain/entity/model.go
@@ -1,10 +1,15 @@
 package entity
 
-type Model struct{
-  Name string
-  MaxTokens int 
+type Model struct {
+	Name      string
+	MaxTokens int
 }
 
-
+func NewModel(name string, maxTokens int) *Model {
+	return &Model{
+		Name:      name,
+		MaxTokens: maxTokens,
+	}
+}
 
 
