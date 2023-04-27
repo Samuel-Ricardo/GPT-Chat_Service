ca65c3d77af24b8ea85683a92c512849f9f4724b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-27 00:27:43 -0300

    :sparkles: create: DTO - ChatCompletionInputDTO

--- internal/domain/usecase/chatcompletionstream/completion.go
+++ internal/domain/usecase/chatcompletionstream/completion.go
@@ -1,16 +1,21 @@
 package chatcompletionstream
 
 type ChatCompletionConfigInputDTO struct {
-  Model string
-  ModelMaxTokenx int
-  Temperature float32
-  TopP float32
-  N int
-  Stop []string
-  MaxTokens int
-  PresencePenalty float32
-  FrequencyPenalty float32
-  InitialSystemMessage string
+	Model                string
+	ModelMaxTokenx       int
+	Temperature          float32
+	TopP                 float32
+	N                    int
+	Stop                 []string
+	MaxTokens            int
+	PresencePenalty      float32
+	FrequencyPenalty     float32
+	InitialSystemMessage string
 }
 
-type
+type ChatCompletionInputDTO struct {
+	ChatID      string
+	UserID      string
+	UserMessage string
+	Config      ChatCompletionConfigInputDTO
+}
