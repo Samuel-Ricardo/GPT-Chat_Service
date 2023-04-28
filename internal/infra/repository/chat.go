a7506997aa72484ef0e7dbb01c04b2a9c2a9db7a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-28 10:01:27 -0300

    :sparkles: create: fun - CreateChat (repository/chat)

--- internal/infra/repository/chat.go
+++ internal/infra/repository/chat.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
+	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/domain/entity"
 	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/infra/db"
 )
 
@@ -18,4 +21,46 @@ func NewChatRepositoryMySQL(database *sql.DB) *ChatRepositoryMySQL {
     DB: database,
     Queries: db.New(database),
   }
+}
+
+func (r *ChatRepositoryMySQL) CreateChat(ctx context.Context, chat *entity.Chat) error {
+  err := r.Queries.CreateChat(
+    ctx,
+    db.CreateChatParams{
+      ID:               chat.ID,
+			UserID:           chat.UserID,
+			InitialMessageID: chat.InitialSystemMessage.Content,
+			Status:           chat.Status,
+			TokenUsage:       int32(chat.TokenUsage),
+			Model:            chat.Config.Model.Name,
+			ModelMaxTokens:   int32(chat.Config.Model.MaxTokens),
+			Temperature:      float64(chat.Config.Temperature),
+			TopP:             float64(chat.Config.TopP),
+			N:                int32(chat.Config.N),
+			Stop:             chat.Config.Stop[0],
+			MaxTokens:        int32(chat.Config.MaxTokens),
+			PresencePenalty:  float64(chat.Config.PresencePenalty),
+			FrequencyPenalty: float64(chat.Config.FrequencyPenalty),
+			CreatedAt:        time.Now(),
+			UpdatedAt:        time.Now(),
+		},
+  )
+
+  if err != nil { return err }
+
+  err = r.Queries.AddMessage(
+    ctx,
+    db.AddMessageParams{
+			ID:        chat.InitialSystemMessage.ID,
+			ChatID:    chat.ID,
+			Content:   chat.InitialSystemMessage.Content,
+			Role:      chat.InitialSystemMessage.Role,
+			Tokens:    int32(chat.InitialSystemMessage.Tokens),
+			CreatedAt: chat.InitialSystemMessage.CreatedAt,
+		},
+  )
+
+  if err != nil {return err}
+
+  return nil
 }
