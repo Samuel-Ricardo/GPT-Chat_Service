90035f6786204f60fea43ae32b148001b45d435e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-28 11:25:20 -0300

    :sparkles: create: DTO - ChatCompletionConfigInputDTO

--- internal/domain/usecase/chatcompletion/completion.go
+++ internal/domain/usecase/chatcompletion/completion.go
@@ -12,3 +12,10 @@ type ChatCompletionConfigInputDTO struct {
 	FrequencyPenalty     float32  // -2.0 to 2.0 - Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, increasing the model's likelihood to talk about new topics.
 	InitialSystemMessage string
 }
+
+type ChatCompletionInputDTO struct {
+  ChatID string `json:"chat_id,omitempty"`
+  UserID string `json:"user_id"`
+  UserMessage string `json:"user_message"`
+  Config ChatCompletionConfigInputDTO `json:"Config"`
+}
