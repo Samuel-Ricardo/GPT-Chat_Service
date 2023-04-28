c01b4ae8f4645d779ec821abc26994a38e2a3af7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-28 09:54:14 -0300

    :sparkles: create: fun - NewChatRepositoryMySQL (repository/chat)

--- internal/infra/repository/chat.go
+++ internal/infra/repository/chat.go
@@ -6,7 +6,16 @@ import (
 	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/infra/db"
 )
 
+
 type ChatRepositoryMySQL struct {
 	DB *sql.DB
   Queries *db.Queries
+}
+
+
+func NewChatRepositoryMySQL(database *sql.DB) *ChatRepositoryMySQL {
+  return &ChatRepositoryMySQL{
+    DB: database,
+    Queries: db.New(database),
+  }
 }
