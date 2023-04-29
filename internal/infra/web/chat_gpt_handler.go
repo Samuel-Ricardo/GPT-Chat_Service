482a9da5ba1e5c2c9b81ca2306c91159655810b8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-29 14:30:00 -0300

    :sparkles: create: fun - WebChatGPTHandler

--- internal/infra/web/chat_gpt_handler.go
+++ internal/infra/web/chat_gpt_handler.go
@@ -1,6 +1,12 @@
 package web
 
-import "github.com/Samuel-Ricardo/GPT-Chat_Service/internal/domain/usecase/chatcompletion"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/domain/usecase/chatcompletion"
+)
 
 type WebChatGPTHandler struct {
 	CompletionUseCase chatcompletion.ChatcompletionUseCase
@@ -14,4 +20,50 @@ func NewWebChatGPTHandler(usecase chatcompletion.ChatcompletionUseCase, config c
     Config: config,
     AuthToken: authToken,
   }
+}
+
+
+func (handler *WebChatGPTHandler) handle(w http.ResponseWriter, r *http.Request) {
+  
+  if r.Method != http.MethodPost {
+    w.WriteHeader(http.StatusMethodNotAllowed)
+    return
+  }
+
+  if r.Header.Get("Authorization") != handler.AuthToken {
+    w.WriteHeader(http.StatusUnauthorized)
+    return
+  }
+
+
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  if !json.Valid(body) {
+    http.Error(w, "Invalid JSON", http.StatusBadRequest)
+    return
+  }
+
+
+  var dto chatcompletion.ChatCompletionInputDTO
+  err = json.Unmarshal(body, &dto)
+    
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+  }
+  
+  dto.Config = handler.Config
+
+  result, err := handler.CompletionUseCase.Execute(r.Context(), dto)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+  
+  w.WriteHeader(http.StatusOK)
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(result)
 }
