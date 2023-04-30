9c655ad93332cada72f455113deb022ffabb5a2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-30 10:31:22 -0300

    :sparkles: create: middleware - Auth (GRPCServer)

--- internal/infra/grpc/server/server.go
+++ internal/infra/grpc/server/server.go
@@ -3,11 +3,17 @@ package server
 import (
 	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/domain/usecase/chatcompletionstream"
 	"github.com/Samuel-Ricardo/GPT-Chat_Service/internal/infra/grpc/service"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+  "google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCServer struct {
 	ChatCompletionStreamUseCase chatcompletionstream.ChatCompletionUseCase
-  ChatConfig                  chatcompletionstream.ChatCompletionConfigInputDTO
+  ChatConfigStream            chatcompletionstream.ChatCompletionConfigInputDTO
   ChatService                 service.ChatService
   Port                        string
   AuthToken                   string
@@ -24,4 +30,26 @@ func NewGRPCServer(chatCompletionStreamUseCase chatcompletionstream.ChatCompleti
 		StreamChannel:               streamChannel,
 		ChatService:                 *chatService,
 	}
+}
+
+func (g *GRPCServer) AuthInterceptor(server interface{}, serverStream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+  ctx := serverStream.Context()
+  
+  md, ok := metadata.FromIncomingContext(ctx)
+  if !ok {
+		return status.Error(codes.Unauthenticated, "metadata is not provided")
+	}
+
+
+  token := md.Get("authorization")
+  if len(token) == 0 {
+		return status.Error(codes.Unauthenticated, "authorization token is not provided")
+	}
+
+
+  if token[0] != g.AuthToken {
+    return status.Error(codes.Unauthenticated, "authorization token is invalid")
+  }
+
+  return handler(server, serverStream)
 }
